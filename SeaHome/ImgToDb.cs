03450370d3547d2eaa2298e76586c56bf3532302
olgaptrova <olgaptrova@gmail.com>
2022-05-15 16:37:26 +0300

    adding files to DB

---
+++ SeaHome/ImgToDb.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeaHome
+{
+    public class ImgToDb
+    {
+        public ObjectId _id { get; set; }
+        public string Name { get; set; }
+        public byte[] img;
+
+        public ImgToDb(string name, byte[] img)
+        {
+            Name = name;
+            this.img = img;
+        }
+
+        public static void AddToDB(ImgToDb img)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images");
+            var collection = database.GetCollection<ImgToDb>("Image");
+            collection.InsertOne(img);
+        }
+
+    }
+}

abfaae1b11539770843e1649a92431d3ebc2ffd9
olgaptrova <olgaptrova@gmail.com>
2022-06-13 11:18:13 +0300

    yandex Map with API

--- SeaHome/wwwroot/script.js
+++ SeaHome/wwwroot/script.js
@@ -1,9 +1,14 @@
 ﻿function initialize()
 {
-    var latlng = new google.maps.LatLng(44.40121, 34.0095);
-    var options = {
-        zoom: 14, center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"), options);
+    //var latlng = new google.maps.LatLng(44.40121, 34.0095);
+    //var options = {
+    //    zoom: 14, center: latlng,
+    //    mapTypeId: google.maps.MapTypeId.ROADMAP
+    //};
+    //var map = new google.maps.Map(document.getElementById("map"), options);
 }
+
+
+
+
+

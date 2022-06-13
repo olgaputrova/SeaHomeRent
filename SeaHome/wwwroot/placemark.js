733a8cddd7fe327c87fd6a3f9cf8a69b93111a05
olgaptrova <olgaptrova@gmail.com>
2022-06-13 11:25:23 +0300

    ymap

--- SeaHome/wwwroot/placemark.js
+++ SeaHome/wwwroot/placemark.js
@@ -13,8 +13,8 @@ function init() {
 
 
     var myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
+        center: [44.40121, 34.0095],
+        zoom: 14
     }, {
         searchControlProvider: 'yandex#search'
     }),
